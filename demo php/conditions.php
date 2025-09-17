643f443ec4e70ac9498dd5aa04371e39d34c753c
carito-cc <carolina.caroca.c@gmail.com>
2025-09-17 20:14:56 +0200

    conditions

--- demo php/conditions.php
+++ demo php/conditions.php
@@ -3,8 +3,8 @@
 /*Utilisez la structure conditionnelle if...else pour afficher un message approprié à partir de la variable 
 suivante :
 $estPresent;
-Si le ministre était présent : affichez "Le ministre était présent".
-Si le ministre n'était pas présent : affichez "Le ministre était absent".
+    Si le ministre était présent : affichez "Le ministre était présent".
+    Si le ministre n'était pas présent : affichez "Le ministre était absent".
 Pour tester les deux situations, modifiez manuellement la valeur de la variable $estPresent.
 Mettez-la d'abord à true pour vérifier le premier cas, puis à false pour vérifier le second.
 */
@@ -24,11 +24,13 @@ echo $text;
 /*Utilisez la structure conditionnelle if... elseif... else pour afficher le message approprié en fonction 
 de la valeur entrée par l'utilisateur à l'aide de la fonction readline() et stockée dans la variable $etatEbriete.
 3 états d'ébriété sont acceptés : (0 = sobre, 1 = joyeux, 2 = ivre) :
+
 $etatEbriete = readline("Quel était l'état d'ébriété du ministre (0 = sobre, 1 = joyeux, 2 = ivre) : ");
-Si l'état d'ébriété est 0, affichez "Le ministre est sobre et responsable !"
-Si l'état d'ébriété est 1, affichez "Le ministre est joyeux !"
-Si l'état d'ébriété est 2, affichez "Le ministre est ivre !"
-Si l'état d'ébriété ne correspond à aucune de ces valeurs, affichez "Erreur ! État d'ébriété inconnu !"
+    Si l'état d'ébriété est 0, affichez "Le ministre est sobre et responsable !"
+    Si l'état d'ébriété est 1, affichez "Le ministre est joyeux !"
+    Si l'état d'ébriété est 2, affichez "Le ministre est ivre !"
+    Si l'état d'ébriété ne correspond à aucune de ces valeurs, affichez "Erreur ! État d'ébriété inconnu !"
+
 Testez les différentes valeurs de la variable $etatEbriete pour vous assurer que les messages s'affichent 
 correctement en fonction de l'état d'ébriété.
 */
